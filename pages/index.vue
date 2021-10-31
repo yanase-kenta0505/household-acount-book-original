9cf8de71ba5817cf13df6f074e43dc7148142395
yanase-kenta0505 <ynsknt0505@gmail.com>
2021-10-31 12:19:00 +0900

    delete : layoutを使用しない方法に切り替え

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,21 @@
+<template>
+  <v-app>
+    <p @click="moveMyPage" class="mx-auto mt-10 text-h6">マイページへ</p>
+  </v-app>
+</template>
 
+<script>
+export default {
+  methods: {
+    moveMyPage() {
+      this.$router.push("/myPage");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  cursor: pointer;
+}
+</style>
