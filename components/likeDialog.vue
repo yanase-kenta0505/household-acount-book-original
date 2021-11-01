f3ebcfe630053851d5bfe566da6281c29a1518cb
yanase-kenta0505 <ynsknt0505@gmail.com>
2021-11-01 20:30:36 +0900

    create : お気に入り確認用ダイアログを作成

---
+++ components/likeDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <div class="text-center">
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-list-item link v-bind="attrs" v-on="on" grey>
+            <v-list-item-icon>
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>お気に入り</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+
+        <v-card width="600px" height="700px">
+          <v-card-title class="text-h5 grey lighten-2">
+            お気に入り
+          </v-card-title>
+
+          <v-card
+            id="follow-card"
+            width="90%"
+            height="auto"
+            class="mx-auto mt-10"
+          >
+            <v-card-title>
+              <v-avatar size="50" color="grey"></v-avatar>
+              <span class="text-subtitle-1 ml-5">けんたけんた</span>
+            </v-card-title>
+            <v-card-text class="mb-5"
+              >ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-icon @click="dialog = false" class="mr-10 mb-5" color="pink"
+                >mdi-chat-outline
+              </v-icon>
+              <v-icon @click="dialog = false" class="mr-10 mb-5" color="pink"
+                >mdi-hand-heart
+              </v-icon>
+            </v-card-actions>
+          </v-card>
+        </v-card>
+      </v-dialog>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-list-item__content {
+  flex: initial;
+}
+</style>
