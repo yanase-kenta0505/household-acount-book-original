e5a2d18968cb188a1f1424bbb8aad68cf63dbe6c
yanase-kenta0505 <ynsknt0505@gmail.com>
2021-11-09 07:03:50 +0900

    fix : カレンダーのイベントクリック時のバブリングを停止

--- pages/test.vue
+++ pages/test.vue
@@ -22,28 +22,6 @@
               {{ $refs.calendar.title }}
             </v-toolbar-title>
             <v-spacer></v-spacer>
-            <!-- <v-menu bottom right>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                  <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right> mdi-menu-down </v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item @click="type = 'day'">
-                  <v-list-item-title>Day</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'week'">
-                  <v-list-item-title>Week</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'month'">
-                  <v-list-item-title>Month</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = '4day'">
-                  <v-list-item-title>4 days</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu> -->
           </v-toolbar>
         </v-sheet>
 
@@ -139,7 +117,6 @@ export default {
     selectDay: null,
     eventName: "",
     amount: null,
-    stopEvent: false,
   }),
   mounted() {
     this.$refs.calendar.checkChange();
@@ -157,47 +134,9 @@ export default {
     next() {
       this.$refs.calendar.next();
     },
-    // showEvent({ nativeEvent, event }) {
-    //   const open = () => {
-    //     this.selectedEvent = event;
-    //     this.selectedElement = nativeEvent.target;
-    //     requestAnimationFrame(() =>
-    //       requestAnimationFrame(() => (this.selectedOpen = true))
-    //     );
-    //   };
 
-    //   if (this.selectedOpen) {
-    //     this.selectedOpen = false;
-    //     requestAnimationFrame(() => requestAnimationFrame(() => open()));
-    //   } else {
-    //     open();
-    //   }
-
-    //   nativeEvent.stopPropagation();
-    // },
     updateRange({ start, end }) {
       const events = [];
-
-      //   const min = new Date(`${start.date}T00:00:00`);
-      //   const max = new Date(`${end.date}T23:59:59`);
-      //   const days = (max.getTime() - min.getTime()) / 86400000;
-      //   const eventCount = this.rnd(days, days + 20);
-
-      //   for (let i = 0; i < eventCount; i++) {
-      //     const allDay = this.rnd(0, 3) === 0;
-      //     const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-      //     const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-      //     const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-      //     const second = new Date(first.getTime() + secondTimestamp);
-
-      //     events.push({
-      //       name: this.names[this.rnd(0, this.names.length - 1)],
-      //       start: first,
-      //       end: second,
-      //       color: this.colors[this.rnd(0, this.colors.length - 1)],
-      //       timed: !allDay,
-      //     });
-      //   }
 
       this.events = events;
     },
@@ -213,8 +152,9 @@ export default {
       });
       this.dialog = false;
     },
-    stop() {
-      this.stopEvent = true;
+    stop({ nativeEvent }) {
+      console.log("stop");
+      nativeEvent.stopPropagation();
     },
   },
 };
