f631a90e3280a8ec0d9024af47ae62b0eaca7a36
yanase-kenta0505 <ynsknt0505@gmail.com>
2021-11-13 21:24:56 +0900

    init : firebaseのdbを構築

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,24 @@
 <template>
-  <v-app> <data-table></data-table> </v-app>
+  <v-app>
+    <v-card width="500px" height="500px" class="mx-auto"> 
+      <v-text-field v-model="text" class="mt-10"></v-text-field>
+      <pre>{{ text }}</pre>
+      <v-btn @click="add">add</v-btn>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import DataTable from "~/components/datatableCard.vue";
 export default {
-  components: { DataTable },
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$store.dispatch("db/add", this.text);
+    },
+  },
 };
 </script>
---
+++ store/db.js
@@ -0,0 +1,14 @@
+import firebase from "~/plugins/firebase";
+
+const db = firebase.firestore();
+const calendarEventRef = db.collection("calendarEvent");
+
+export const state = () => ({});
+
+export const getters = {};
+
+export const actions = {
+  
+};
+
+export const mutations = {};
