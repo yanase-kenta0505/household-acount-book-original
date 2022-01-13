ac05e005a7e7223d934fb48017160fe69f5e59c2
yanase-kenta0505 <ynsknt0505@gmail.com>
2022-01-13 22:29:27 +0900

    fix : 投稿一覧にlikeCountを反映

--- components/postList.vue
+++ components/postList.vue
@@ -27,7 +27,7 @@
           width="90%"
           height="auto"
           class="mx-auto mt-10"
-          v-for="(message) in myPostMessages"
+          v-for="message in myPostMessages"
           :key="message.id"
         >
           <v-card-title>
@@ -44,12 +44,20 @@
           <v-card-text class="mb-5"> {{ message.message }}</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-icon @click="dialog = false" class="mr-10 mb-5" color="pink"
+            <v-icon @click="dialog = false" class="mr-10" color="grey"
               >mdi-chat-outline
             </v-icon>
-            <v-icon @click="dialog = false" class="mr-10 mb-5" color="pink"
-              >mdi-hand-heart
-            </v-icon>
+
+            <div class="mr-7">
+              <v-icon
+                class="mb-1 pink"
+                @click="changeLikeCount(message)"
+                :class="[chack_liking(message) ? 'pink' : 'grey']"
+                >mdi-hand-heart
+              </v-icon>
+
+              <span>{{ message.likeCount }}</span>
+            </div>
           </v-card-actions>
         </v-card>
       </v-card>
@@ -79,6 +87,22 @@ export default {
       });
       return myPostMessages;
     },
+    likingPostIds() {
+      const a = JSON.parse(
+        JSON.stringify(this.$store.state.like.likingPostIds)
+      );
+
+      let allIds = [];
+      a.forEach((id) => {
+        allIds.push(id.likingPostId);
+      });
+      return allIds;
+    },
+    chack_liking() {
+      return function (message) {
+        return this.likingPostIds.includes(message.id);
+      };
+    },
   },
 
   methods: {
@@ -89,13 +113,32 @@ export default {
         id: message.id,
       });
     },
+
+    changeLikeCount(message) {
+      this.$store.dispatch("postDB/changeLikeCount", {
+        id: message.id,
+        uid: this.$router.currentRoute.params.id,
+      });
+    },
   },
 };
 </script>
 
-
 <style lang="scss" scoped>
-  ::v-deep .v-avatar{
-    background-size: cover;
+::v-deep .v-avatar {
+  background-size: cover;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
   }
+}
+
+::v-deep .theme--light.v-icon.pink {
+  color: rgb(236, 61, 90);
+  background-color: transparent !important;
+}
+::v-deep .theme--light.v-icon.grey {
+  color: grey;
+  background-color: transparent !important;
+}
 </style>
